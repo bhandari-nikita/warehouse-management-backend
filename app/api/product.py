b81d0996a88a5a99c880b0c2e50277a0427c1206
niky1859 <nikitabhandari018@gmail.com>
2026-05-17 10:51:28 +0530

    implement update delete filtering and pagination APIs

--- app/api/product.py
+++ app/api/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 
 from app.db.dependencies import get_db
@@ -6,6 +6,20 @@ from app.models.product import Product
 from app.schemas.product import (ProductCreate, ProductResponse)
 
 router = APIRouter (prefix="/products", tags=["Products"])
+
+@router.get("/", response_model=list[ProductResponse])
+def get_products(
+    min_price: float = 0,
+    limit: int = 10,
+    offset: int = 0,
+    db: Session = Depends(get_db)
+):
+    products = db.query(Product).filter(
+        Product.price >= min_price
+    ).offset(offset).limit(limit).all()
+
+    return products
+
 
 @router.post("/", response_model=ProductResponse)
 def create_product(
@@ -25,8 +39,50 @@ def create_product(
 
     return new_product
 
-@router.get("/", response_model=list[ProductResponse])
-def get_product(db: Session = Depends(get_db)):
-    products = db.query(Product).all()
 
-    return products
+@router.put("/{product_id}", response_model=ProductResponse)
+def update_product(
+    product_id: int,
+    updated_product: ProductCreate,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Product not found"
+        )
+    
+    product.name = updated_product.name,
+    product.sku = updated_product.sku,
+    product.description = updated_product.description,
+    product.price = updated_product.price
+
+    db.commit()
+    db.refresh(product)
+
+    return product
+
+
+@router.delete("/{product_id}")
+def delete_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Product not found"
+        )
+    
+    db.delete(product)
+    db.commit()
+
+    return {"message": "Product deleted successfully"}
